06ac585571c6d3ae8163520d3e864364404427df
samet <samet.mollaoglu85@gmail.com>
2024-02-19 17:44:26 +0300

    02-variables lesson

---
+++ 02-variables/sample-types.js
@@ -0,0 +1,13 @@
+var found = true;
+var grade = 88.6;
+var firstName = "Anup";
+var lastName = "Kumar";
+/*
+found = 0;
+grade = "A";
+firstName = false;
+*/
+console.log(found);
+console.log("The grade is " + grade);
+console.log("Hi " + firstName + " " + lastName);
+console.log("Hi ".concat(firstName, " ").concat(lastName));
